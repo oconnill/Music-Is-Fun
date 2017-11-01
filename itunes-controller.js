06c61583354dd61b7f29c85b12e5af7871a197c8
Daniel O'Connell <doconnelldesign@gmail.com>
2017-11-01 11:58:42 -0600

    basic outline html

--- itunes-controller.js
+++ itunes-controller.js
@@ -8,7 +8,8 @@ function ItunesController(){
   }
 
   //Start coding here
-  function draw(){
+  function draw(songList){
+  console.log(songList)
   }
 
 
@@ -16,3 +17,46 @@ function ItunesController(){
 
   
 }
+
+
+
+// {title: "Country Grammar (Hot...)", albumArt: "http://is3.mzstatic.com/image/thumb/Music/v4/2e/84…7a-8251-9f21-89c6-172c58d4ddda/source/60x60bb.jpg", artist: "Nelly", collection: "Country Grammar", price: 9.99, …}
+
+// cats = []
+
+// function Cat(name, age, personality, image) {
+//     this.name = name,
+//         this.age = age,
+//         this.personality = personality || 'grumpy'
+//     this.image = image || 'http://lorempixel.com/200/200/cats/'
+}
+
+// var garfield = new Cat('Garfield', 22, 'lazy', 'https://pbs.twimg.com/profile_images/451027744361422849/f5KCfGHa_200x200.jpeg')
+// var mittens = new Cat('Mittens', 2)
+// var snibbly = new Cat('Mr. Snibbly', 152)
+// cats.push(garfield, mittens, snibbly)
+
+// function draw(arr) {
+//     var template = ''
+//     for (var i = 0; i < arr.length; i++) {
+//         var cat = arr[i]
+//         template += `
+//         	<div class="col s3 red-border">
+// 				<img src="${cat.image}">
+// 				<h4>${cat.name}</h4>
+// 				<h6>${cat.age}</h6>
+// 				<h6>${cat.personality}</h6>
+//                 <button onclick="meow('${cat.name}')">MEOW!</button>
+// 			</div>
+//         `
+//     }
+//     document.getElementById('my-cats').innerHTML = template
+// }
+
+// globals.meow = function meow(catName){
+//     console.log(catName, 'Meow')
+// }
+
+
+
+// draw(cats)
